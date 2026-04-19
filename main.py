93cfeea04f9752956dcdb579daa42053df060ee8
Abdul Rehman <abdurehman917@gmail.com>
2026-04-19 13:13:57 +0500

    master - first day basics

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello, World!')
+# Init
+
+x=15
+def addition(y, x_assign):
+    x=x_assign
+    # string literals
+    print(f"Adding {x} to {y} is: ", x+y, " With Type: ", type(x+y))
+
+# inputs
+input_assign = input('Enter the second number to add: ')
+# typecasting with eval
+addition(5, eval(input_assign))
